cef76305284bc9abdf3f731e87222f863f5ff02f
keyemsi <99300353+keyemsi@users.noreply.github.com>
2022-03-25 15:41:56 -0700

    Add files via upload

---
+++ programa6.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+#include <math.h>
+#include <string>
+/*
+*Problema 6
+*Imprimir la tabla de multiplicar
+*hasta 12x12
+*
+*/
+//@Author       Fabian Acosta Gutierrez 19211591
+// @Date        25 marzo 2022
+using namespace std;
+
+int main()
+ {
+float dato1=0,dato2=0;
+        for(int i=1;i<13;i++)
+        {
+                for(int z=1;z<13;z++ )
+                {
+                cout<<i<<" x "<<z<<" = "<<z*i<< endl;
+
+                }
+
+        }
+
+return 0;
+
+}

e25a3411c5a7a8726b944ad9f9c1e8e461900ec6
keyemsi <99300353+keyemsi@users.noreply.github.com>
2022-03-25 11:35:35 -0700

    Add files via upload

---
+++ Programa1.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+#include <math.h>
+
+/*
+*Problema 1
+*Introducir un conjunto 25 numeros.
+*Determinar la cantidad de numeros positivos y
+*negativos del conjunto
+*/
+//@Author       Fabian Acosta Gutierrez 19211591
+// @Date        25 marzo 2022
+using namespace std;
+
+int main() {
+
+float dato,fin;
+int Neg=0,Pos=0;
+for(int i=0;i<25;i++)
+        {
+        cout << "Ingresa un numero positvo o negativo" <<endl;
+        cin >> dato;
+        if (dato>0)
+         {
+        Pos++;
+         }
+        else
+         {
+        Neg++;
+         }
+
+        }
+  cout << "Numero de positivos "<< Pos <<endl;
+  cout << "Numero de negativos "<< Neg <<endl;
+
+  return 0;
+}

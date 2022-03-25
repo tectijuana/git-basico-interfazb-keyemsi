07b8bef42dc5e0c493a54cb17b09d093125c049d
keyemsi <99300353+keyemsi@users.noreply.github.com>
2022-03-25 15:34:49 -0700

    Add files via upload

---
+++ programa5.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+#include <math.h>
+#include <string>
+/*
+*Problema 5
+*Imprimir la tabla de sumar
+*hasta 12+12
+*
+*/
+//@Author       Fabian Acosta Gutierrez 19211591
+// @Date        25 marzo 2022
+using namespace std;
+
+int main()
+ {
+float dato1=0,dato2=0;
+        for(int i=1;i<13;i++)
+        {
+                for(int z=1;z<13;z++ )
+                {
+                cout<<i<<" + "<<z<<" = "<<z+i<< endl;
+
+                }
+
+        }
+
+return 0;
+
+}

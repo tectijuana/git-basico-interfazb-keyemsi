151e2545f77b93eeabcd30f9fc0164d15a5affb9
keyemsi <99300353+keyemsi@users.noreply.github.com>
2022-03-25 16:49:50 -0700

    Add files via upload

---
+++ programa8.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <math.h>
+#include <string>
+/*
+*Problema 8
+*Encontrar la suma de 35 numeros
+*
+*
+*/
+//@Author       Fabian Acosta Gutierrez 19211591
+// @Date        25 marzo 2022
+using namespace std;
+int main()
+{
+int dato1=0,dato2=0;
+        for(int i=0;i<35;i++)
+        {
+        cout << "ingresa numero entero " << endl;
+        cin >> dato1;
+        dato2=dato1+dato2;
+        }
+cout<<"resultado de la suma "<<dato2<<endl;
+return 0;
+}
+

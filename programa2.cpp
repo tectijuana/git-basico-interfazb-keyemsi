d8b8558d8c1283933434a8c86e3f0842444b526b
keyemsi <99300353+keyemsi@users.noreply.github.com>
2022-03-25 17:28:33 -0700

    Update programa2.cpp

--- programa2.cpp
+++ programa2.cpp
@@ -6,7 +6,7 @@
 *en una lista de quince enteros              
 *algunos de los cuales son impares
 */
-//@Author       Fabian Acosta Gutierrez 19211591
+//@Author       keyemsi
 // @Date        25 marzo 2022
 
 using namespace std;

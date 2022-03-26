7af2aee02d562a218e87b095605a8b28a3b039f3
keyemsi <99300353+keyemsi@users.noreply.github.com>
2022-03-25 18:31:15 -0700

    Add files via upload

---
+++ programa11.cpp
@@ -0,0 +1,39 @@
+#include <iostream> 
+#include <math.h>
+#include <string>
+/*
+*Problema 11
+*
+*
+*
+*/
+//@Author       keyemsi
+// @Date        25 marzo 2022
+using namespace std;
+int main()
+{
+double num1=0,num2=0,res=0;
+cout<< "ingresa un numero cualquiera "<<endl ;
+cin>>num1;
+cout<< "ingresa otro numero cualquiera "<<endl ;
+cin>>num2;
+
+res=num1+num2;
+
+        if(res>0)
+        {
+                if(res=0)
+                {
+                cout<<"es igual a cero"<<endl;
+                }
+        cout<<"es positiva"<<endl;
+        }
+        else
+        {
+        cout<<"es negativa"<<endl;
+        }
+
+return 0;
+}
+
+

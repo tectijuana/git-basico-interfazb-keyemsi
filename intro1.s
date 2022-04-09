4fd49e0ce7e5948d55195f5aeb3cb6dfa2195667
keyemsi <99300353+keyemsi@users.noreply.github.com>
2022-04-09 02:24:26 -0700

    Create intro1.s

---
+++ intro1.s
@@ -0,0 +1,19 @@
+.data
+var1:   .word   3
+var2:   .word   4
+var3:   .word   0x1234
+
+.text
+.global main
+main:   ldr     r1, puntero_var1
+        ldr     r1,[r1]
+        ldr     r2,puntero_var2
+        ldr     r2,[r2]
+        ldr     r3,puntero_var3
+        add     r0,r1,r2
+        str     r0,[r3]
+        bx      lr
+
+puntero_var1:   .word   var1
+puntero_var2:   .word   var2
+puntero_var3:   .word   var3
